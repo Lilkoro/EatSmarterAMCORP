532fb01ffa6b076b42ba44944ee338e5eae0f383
Niou <amirgouadjelia97@gmail.com>
2024-11-22 00:23:10 +0100

    Création de la page CSS et stylisation de la page "Message bien reçu"

--- EatSmart V3/contact.php
+++ EatSmart V3/contact.php
@@ -1,28 +1,43 @@
-
-
-
 <?php
 if(isset($_GET["email"])){
-    echo "<h1>Message bien reçu ! </h1>";
-    echo "<h3>Pour rappel des infos : </h3>";
-    echo "</br>Votre e-mail est ".$_GET["email"]."</br>";
-    echo "Message : ".$_GET["mess"];
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylecontact.css" />
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+        <h2>Contactez nous !</h2>
+        <h1>Message bien reçu !</h1>
+        <h3>Pour rappel des infos : </h3>';
+        echo '</br>Votre nom est : '.$_GET["Nom"].'</br>';
+        echo '</br>Votre e-mail est '.$_GET["email"].'</br>';
+        echo 'Message : '.$_GET["msg"];
+    echo '</div>
+</body>
+</html>';
 } else {
     echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylecontact.css" />
     <title>Document</title>
 </head>
 <body>
-    <form id="pre" action="" method="get">
-        <label for="email">Votre e-mail</label>    <br>
-        <input type="email" id="email" name="email" require>    <br>
-        <label for="mess">Votre message : </label>    <br>
-        <input type="text" name="mess" id="mess" require>    <br>
-    </form>
-    <input type="submit" form="pre" value="Envoyez">
+<div class="container">
+        <h2>Contactez nous !</h2>
+        <form action="">
+            <input type="text" name="Nom" id="Nom" placeholder="Nom" required>
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            <textarea name="msg" id="msg" cols="30" rows="10" placeholder="Votre message..."></textarea>
+            <button type="submit">Envoyer</button>
+        </form>
+    </div>
 </body>
 </html>';
 }
