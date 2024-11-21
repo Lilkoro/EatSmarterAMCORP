fef79a2e0ef33109ecc2d1ad29d5bd22859eda18
Lilkoro <devlilkoro@gmail.com>
2024-11-21 15:08:42 -0600

    Modification pour afficher l'essentiel

--- EatSmart v1.0/menu.php
+++ EatSmart v1.0/menu.php
@@ -103,146 +103,6 @@
     </div>
 
 
-    <!-- our chef -->
-
-
-    <div class="container-fluid image-back-1 bg-danger-subtle" id="about">
-        <div class="container">
-            <div class="row pt-5">
-                <div class="col">
-                    <div class="h1 text-danger-emphasis text-center">Our Chef</div>
-                </div>
-                <div class="row">
-                    <div class="col-md-8 offset-md-2 text-center text-danger-emphasis">Lorem ipsum dolor sit, amet
-                        consectetur adipisicing elit. Mollitia
-                        libero odio quam,Alias autem culpa quam Alias autem culpa quam et eius ducimus temporibus Alias
-                        autem culpa quam ea magni </div>
-                </div>
-            </div>
-            <div class="row mt-5 pb-5">
-                <div class="col-md-4 col-sm-12">
-                    <div class="card-chef text-center bg-transparent border-0">
-                        <div class="card-img-top">
-                            <img class="w-50" src="../Assestss/chef 1.png" alt="Loading...">
-                            <div class="card-body">
-                                <h3 class="text-danger-emphasis">AIDEN HUNTER</h3>
-                                <p class="text-danger-emphasis fs-4">Founder</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="card-chef text-center  bg-transparent border-0">
-                        <div class="card-img-top">
-                            <img class="w-50" src="../Assestss/chef 2.png" alt="Loading...">
-                            <div class="card-body">
-                                <h3 class="text-danger-emphasis">ETHEL RAMSEY</h3>
-                                <p class="text-danger-emphasis fs-4">Co-Founder</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="card-chef text-center  bg-transparent border-0">
-                        <div class="card-img-top">
-                            <img class="w-50" src="../Assestss/chef 3.png" alt="Loading...">
-                            <div class="card-body">
-                                <h3 class="text-danger-emphasis">FANNIE STEWART</h3>
-                                <p class="text-danger-emphasis fs-4">Co-Founder</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <!-- Best Burger -->
-
-
-    <div class="intro" id="product">
-        <div class="card container border-0 pt-5 pb-5 bg-transparent">
-            <div class="row g-0 justify-content-around">
-                <div class="col-lg-6 text-center">
-                    <img src="../Assestss/8.png" class="img-fluid w-50 ms-4" alt="Frame...">
-                </div>
-                <div class="card-body col-lg-6 pt-5">
-                    <h1 class="card-title-intro fw-bold text-white">Best Burger</h1>
-                    <p class="card-text fs-4 text-white">lorem ipsum Laborum veritatis soluta, consequatur magni
-                        doloribus shshsh shshsh tempora
-                        esse!</p>
-                    <button type="button"
-                        class="btn-1 text-danger bg-light border-0 rounded-5 pe-4 ps-4 mt-3 fs-3">Order
-                        Now</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <!-- big burger -->
-
-
-    <div class="container-fluid image-back-1-1 bg-danger-subtle">
-        <div class="card container border-0 pt-5 pb-5 bg-transparent">
-            <div class="row g-0 justify-content-around">
-                <div class="card-body col-lg-6">
-                    <h1 class="card-title-intro fw-bold text-danger-emphasis pt-4">Big Burger</h1>
-                    <p class="card-text fs-4 text-danger-emphasis">lorem ipsum Laborum veritatis soluta, consequatur
-                        magni doloribus shshsh shshsh tempora esse veritatis soluta, consequatur
-                        magni doloribus shshsh shshsh tempora esse</p>
-                    <button type="button" class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 mt-3 fs-3">Order
-                        Now</button>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <img src="../Assestss/8.png" class="img-fluid w-50 " alt="Frame...">
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <!-- statistics -->
-
-
-    <div class="intro-3" id="promo">
-        <div class="container">
-            <div class="row pt-5">
-                <div class="col">
-                    <div class="h1 text-white text-center">Statistics</div>
-                </div>
-                <div class="row">
-                    <div class="col-md-8 offset-md-2 text-center text-white">Lorem ipsum dolor sit, amet
-                        consectetur adipisicing elit. Mollitia
-                        libero odio quam,Alias autem culpa quam Alias autem culpa quam et eius ducimus temporibus Alias
-                        autem culpa quam ea magni </div>
-                </div>
-            </div>
-            <div class="row pt-5 gy-5 pb-5">
-                <div class="col-lg-4 col-md-3">
-                    <div class="circle-1 text-white">
-                        <h1 class="fs-1 pb-2 fw-bold">7K</h1>
-                        <p class="fs-5">Customer</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-3">
-                    <div class="circle-1 text-white">
-                        <h1 class="fs-1 pb-2 fw-bold">109</h1>
-                        <p class="fs-5">Outlets</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-3">
-                    <div class="circle-1 text-white">
-                        <h1 class="fs-1 pb-2 fw-bold">35</h1>
-                        <p class="fs-5">Country</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
     <!-- Our Packages -->
 
 
@@ -256,19 +116,11 @@
             <div class="row row-cols-1 row-cols-md-3 gy-4 pb-5">
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center text-danger pt-3 pb-3"><?php echo $nomMenu["M1"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["M1"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["M1"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center text-danger pt-3 pb-3">Menu S</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">7.90Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Comprend 5 Tenders et 8 Hot Wings avec une boisson.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -276,19 +128,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-danger text-center pt-3 pb-3"><?php echo $nomMenu["M2"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["M2"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["M2"];?></p>
-=======
-                        <h4 class="card-title text-danger text-danger text-center pt-3 pb-3">Bucket M</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">15.90Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Comprend 10 Tenders et 16 Hot Wings avec une boisson.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -296,19 +140,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center pt-3 pb-3"><?php echo $nomMenu["M3"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["M3"];?></h5>
                             <p class="card-text text-danger ps-3 pe-3"><?php echo $descrMenu["M3"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center pt-3 pb-3">Bucket L</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">23.90Euro</h5>
-                            <p class="card-text text-danger ps-3 pe-3">Comprend 20 Tenders et 32 Hot Wings avec une boisson.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -324,19 +160,11 @@
             <div class="row row-cols-1 row-cols-md-3 gy-4 pb-5">
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center text-danger pt-3 pb-3"><?php echo $nomMenu["B1"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["B1"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["B1"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center text-danger pt-3 pb-3">Double Krunch® </h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">5.50Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Un burger croustillant avec du fromage, sans tomate, inclus avec boisson et frites.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -344,19 +172,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-danger text-center pt-3 pb-3"><?php echo $nomMenu["B2"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["B2"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["B2"];?></p>
-=======
-                        <h4 class="card-title text-danger text-danger text-center pt-3 pb-3">Colonel Original®</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">6.90Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Burger avec filet de poulet croustillant et fromage, avec option bacon (+0,50 €), inclus avec boisson et frites.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -364,19 +184,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center pt-3 pb-3"><?php echo $nomMenu["B3"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["B3"];?></h5>
                             <p class="card-text text-danger ps-3 pe-3"><?php echo $descrMenu["B3"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center pt-3 pb-3">Double Stacker</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">7.50Euro</h5>
-                            <p class="card-text text-danger ps-3 pe-3">Burger au poulet avec fromage et bacon, inclus avec boisson et frites.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -393,19 +205,11 @@
             <div class="row row-cols-1 row-cols-md-3 gy-4 pb-5">
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center text-danger pt-3 pb-3"><?php echo $nomMenu["D1"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["D1"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["D1"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center text-danger pt-3 pb-3">Muffin au Nutella</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">2.20Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Muffin moelleux au Nutella.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -413,19 +217,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-danger text-center pt-3 pb-3"><?php echo $nomMenu["D2"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["D2"];?></h5>
                             <p class="card-text text-danger pe-3 ps-3"><?php echo $descrMenu["D2"];?></p>
-=======
-                        <h4 class="card-title text-danger text-danger text-center pt-3 pb-3">Kream Ball®</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">2.00Euro</h5>
-                            <p class="card-text text-danger pe-3 ps-3">Crème glacée en coupe. </p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
@@ -433,19 +229,11 @@
                 </div>
                 <div class="col">
                     <div class="card card-pack">
-<<<<<<< HEAD
                         <h4 class="card-title text-danger text-center pt-3 pb-3"><?php echo $nomMenu["D3"];?></h4>
                         <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
                         <div class="card-body text-center">
                             <h5 class="card-text text-danger fs-4"><?php echo $prixMenu["D3"];?></h5>
                             <p class="card-text text-danger ps-3 pe-3"><?php echo $descrMenu["D3"];?></p>
-=======
-                        <h4 class="card-title text-danger text-center pt-3 pb-3">Pom'Potes® Bio</h4>
-                        <img src="../Assestss/10.png" class="card-img w-25 align-self-center" alt="Loading...">
-                        <div class="card-body text-center">
-                            <h5 class="card-text text-danger fs-4">1.50Euro</h5>
-                            <p class="card-text text-danger ps-3 pe-3">Compote de pommes bio.</p>
->>>>>>> 97a09137f8499990974b8c77d28dc872aa471d1a
                             <button type="button"
                                 class="btn bg-danger text-white border-0 rounded-5 pe-4 ps-4 fs-4">Ajout au Panier</button>
                         </div>
