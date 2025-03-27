6f60e2c072696f04b97049df2271cb1687c663dc
Lilkoro <devlilkoro@gmail.com>
2025-03-27 06:39:21 -0600

    Correction des erreurs d'encodages

--- EatSmart v4.0/config/connection.php
+++ EatSmart v4.0/config/connection.php
@@ -3,8 +3,8 @@ try {
     $dns = "mysql:host=127.0.0.1;dbname=eatsmart;port=3306";
     $user = "root";
     $pass = "";
-    $con = new PDO($dns,$user,$pass);
-    // Configurer PDO pour gérer les erreurs en mode Exception
+    $con = new PDO($dns, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+    // Configurer PDO pour gérer les erreurs en mode Exception et l'utf8
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo "erreur => ". $e;
--- EatSmart v4.0/config/footer.php
+++ EatSmart v4.0/config/footer.php
@@ -10,7 +10,7 @@
 <footer class="bg-body-tertiary text-center text-lg-start">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2020 Copyright: AMCorp <br>
+    © 2025 Copyright: AMCorp <br>
     Marc et Amir : MarcAmir@AMCorp.com
   </div>
   <!-- Copyright -->
